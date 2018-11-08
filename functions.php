77e96ae3c4a86f2c4b55abf1c8ed627bdb2ff1df
Adam <rumpelteam@gmail.com>
2018-11-08 16:21:03 +0100

    Add some test widget

--- functions.php
+++ functions.php
@@ -1,6 +1,58 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
+function my_theme_enqueue_styles () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function ak_load_widget () {
+    register_widget( 'ak_widget' );
+}
+
+add_action( 'widgets_init', 'ak_load_widget' );
+
+class ak_widget extends WP_Widget {
+    function __construct()
+    {
+        parent::__construct(
+            'ak_widget',
+            __('WPBeginner Widget', 'wpb_widget_domain'),
+            array( 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'wpb_widget_domain' ), )
+        );
+    }
+
+    public function widget( $args, $instance ) {
+        $title = apply_filters( 'widget_title', $instance['title'] );
+
+// before and after widget arguments are defined by themes
+        echo $args['before_widget'];
+        if ( ! empty( $title ) )
+            echo $args['before_title'] . $title . $args['after_title'];
+
+// This is where you run the code and display the output
+        echo __( 'Hello, World!', 'wpb_widget_domain' );
+        echo $args['after_widget'];
+    }
+
+// Widget Backend
+    public function form( $instance ) {
+        if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+        }
+        else {
+            $title = __( 'New title', 'wpb_widget_domain' );
+        }
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+        </p>
+        <?php
+    }
+
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        return $instance;
+    }
 }
 ?>
